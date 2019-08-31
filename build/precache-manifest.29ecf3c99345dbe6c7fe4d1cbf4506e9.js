e7efb3528847a6e0dbd688066c3e0ca6d5b999b0
BiggaHD <dzenis.hankusic@gmail.com>
2019-08-31 19:46:37 +0200

    Add build folder

---
+++ build/precache-manifest.29ecf3c99345dbe6c7fe4d1cbf4506e9.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de903fc2eed944bab8932b336906f581",
+    "url": "/reactivePortfolio/index.html"
+  },
+  {
+    "revision": "8c42a766525d1373d726",
+    "url": "/reactivePortfolio/static/css/main.09f588b3.chunk.css"
+  },
+  {
+    "revision": "52e3d826294ed1e05532",
+    "url": "/reactivePortfolio/static/js/2.a2ad9e51.chunk.js"
+  },
+  {
+    "revision": "8c42a766525d1373d726",
+    "url": "/reactivePortfolio/static/js/main.08678019.chunk.js"
+  },
+  {
+    "revision": "0034e56c75a87292dcd0",
+    "url": "/reactivePortfolio/static/js/runtime~main.4da91a3f.js"
+  },
+  {
+    "revision": "827a100ec9b449a58a05feece985c6d0",
+    "url": "/reactivePortfolio/static/media/background.827a100e.jpeg"
+  },
+  {
+    "revision": "fe933858da6910cc2aa3d2c1b8de585a",
+    "url": "/reactivePortfolio/static/media/p1.fe933858.JPG"
+  },
+  {
+    "revision": "109de6cc9d60f9c519099474e66423e5",
+    "url": "/reactivePortfolio/static/media/p10.109de6cc.JPG"
+  },
+  {
+    "revision": "1814b6bc3b083f8c266b1c306dc3d35b",
+    "url": "/reactivePortfolio/static/media/p11.1814b6bc.jpg"
+  },
+  {
+    "revision": "c71e236586793970b6fa00035927bea6",
+    "url": "/reactivePortfolio/static/media/p12.c71e2365.jpg"
+  },
+  {
+    "revision": "866523eeba868979acce0b0bf6b005dd",
+    "url": "/reactivePortfolio/static/media/p13.866523ee.jpg"
+  },
+  {
+    "revision": "be3b525b90274a84d92fee63aa5e44c2",
+    "url": "/reactivePortfolio/static/media/p14.be3b525b.jpg"
+  },
+  {
+    "revision": "937a6f297938c59defd29aa8d62e2c60",
+    "url": "/reactivePortfolio/static/media/p15.937a6f29.png"
+  },
+  {
+    "revision": "04e9e6c03ad97ca4e8885bf85d51464c",
+    "url": "/reactivePortfolio/static/media/p16.04e9e6c0.png"
+  },
+  {
+    "revision": "e6833d67a694f22183db0c98bd8413ca",
+    "url": "/reactivePortfolio/static/media/p17.e6833d67.png"
+  },
+  {
+    "revision": "9dfbbdce99b43c5763473ce82e0315f3",
+    "url": "/reactivePortfolio/static/media/p18.9dfbbdce.jpg"
+  },
+  {
+    "revision": "578e738bced0b4adff91e2a0f84751a2",
+    "url": "/reactivePortfolio/static/media/p2.578e738b.png"
+  },
+  {
+    "revision": "dae553c5fc1a1102992a5436a612dd20",
+    "url": "/reactivePortfolio/static/media/p3.dae553c5.png"
+  },
+  {
+    "revision": "afe9b1aaf6f0e12e670ddce60c5358dc",
+    "url": "/reactivePortfolio/static/media/p4.afe9b1aa.png"
+  },
+  {
+    "revision": "7e51221633b0ae91cb99569abb9ceb39",
+    "url": "/reactivePortfolio/static/media/p5.7e512216.png"
+  },
+  {
+    "revision": "81b9aa74bd9292f282c3f86489cac227",
+    "url": "/reactivePortfolio/static/media/p6.81b9aa74.png"
+  },
+  {
+    "revision": "8757d935b7d69954f6b5a5858b7b85e5",
+    "url": "/reactivePortfolio/static/media/p7.8757d935.jpg"
+  },
+  {
+    "revision": "160760da53ff2aa10fbbfeb96ba48d19",
+    "url": "/reactivePortfolio/static/media/p8.160760da.jpg"
+  },
+  {
+    "revision": "53ae8a545e53433626176d88e8e67874",
+    "url": "/reactivePortfolio/static/media/p9.53ae8a54.jpg"
+  },
+  {
+    "revision": "84a38f1d7353021c46efbe875c3f8e88",
+    "url": "/reactivePortfolio/static/media/portrait.84a38f1d.jpg"
+  },
+  {
+    "revision": "cae9d5cdc1095f73cacd27b639ee6126",
+    "url": "/reactivePortfolio/static/media/portrait_small.cae9d5cd.jpg"
+  }
+]);
